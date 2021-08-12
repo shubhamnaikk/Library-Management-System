e6e5e45bfb9e9a5118249cd5d7e9aec1de45b472
shubhamnaikk <nshubham48@gmail.com>
2021-08-12 21:59:33 +0530

    first commit

---
+++ code/ReturnBook.py
@@ -0,0 +1,113 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+import pymysql
+
+# Add your own database name and password here to reflect in the code
+mypass = "root"
+mydatabase="db"
+
+con = pymysql.connect(host="localhost",user="root",password="xyz",database=mydatabase)
+cur = con.cursor()
+
+# Enter Table Names here
+issueTable = "books_issued" #Issue Table
+bookTable = "books" #Book Table
+
+
+allBid = [] #List To store all Book IDs
+
+def returnn():
+    
+    global SubmitBtn,labelFrame,lb1,bookInfo1,quitBtn,root,Canvas1,status
+    
+    bid = bookInfo1.get()
+
+    extractBid = "select bid from "+issueTable
+    try:
+        cur.execute(extractBid)
+        con.commit()
+        for i in cur:
+            allBid.append(i[0])
+        
+        if bid in allBid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'issued':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+    
+    
+    issueSql = "delete from "+issueTable+" where bid = '"+bid+"'"
+  
+    print(bid in allBid)
+    print(status)
+    updateStatus = "update "+bookTable+" set status = 'avail' where bid = '"+bid+"'"
+    try:
+        if bid in allBid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success',"Book Returned Successfully")
+        else:
+            allBid.clear()
+            messagebox.showinfo('Message',"Please check the book ID")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error","The value entered is wrong, Try again")
+    
+    
+    allBid.clear()
+    root.destroy()
+    
+def returnBook(): 
+    
+    global bookInfo1,SubmitBtn,quitBtn,Canvas1,con,cur,root,labelFrame, lb1
+    
+    root = Tk()
+    root.title("Library")
+    root.minsize(width=400,height=400)
+    root.geometry("600x500")
+
+    
+    Canvas1 = Canvas(root)
+    
+    Canvas1.config(bg="#272727")
+    Canvas1.pack(expand=True,fill=BOTH)
+        
+    headingFrame1 = Frame(root)
+    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
+        
+    headingLabel = Label(headingFrame1, text="Return Book", bg='#F27B35', fg='white',font=('open sans bold',18))
+    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+    
+    labelFrame = Frame(root,bg='#272727')
+    labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)   
+        
+    # Book ID to Delete
+    lb1 = Label(labelFrame,text="Book ID : ", bg='#272727', fg='white')
+    lb1.place(relx=0.05,rely=0.5)
+        
+    bookInfo1 = Entry(labelFrame)
+    bookInfo1.place(relx=0.3,rely=0.5, relwidth=0.62)
+    
+    #Submit Button
+    SubmitBtn = Button(root,text="Return",bg='#00c4ff', fg='white',font=('open sans bold',10),command=returnn)
+    SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    quitBtn = Button(root,text="Quit",bg='#00c4ff', fg='white',font=('open sans bold',10), command=root.destroy)
+    quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    root.mainloop()
